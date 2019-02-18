b529fe5e341189dfe44a1e5eef1204a1fbbd90d2
leeks <lilim2019@u.northwestern.edu>
2019-02-18 13:49:01 -0600

    new dfs commit

--- student_code_uninformed_solvers.py
+++ student_code_uninformed_solvers.py
@@ -19,36 +19,38 @@ class SolverDFS(UninformedSolver):
             True if the desired solution state is reached, False otherwise
         """
         ### Student code goes here
-        possible_moves = self.gm.getMovables  # returns a list of movable Statements
-        child_depth = self.gm.depth + 1
-        for move in possible_moves:  # build list of children in this new node
-            self.gm.makeMove(move)  # try expanding into this state to see if it is new
-            if self.gm.getGameState() not in self.visited:
-                new_state = GameState(self.gm.getGameState, child_depth, move)
+        if self.currentState.state == self.victoryCondition:   # first check if already resolved
+            return True
+        possible_moves = self.gm.getMovables()  # returns a list of movable Statements
+        child_depth = self.currentState.depth + 1
+        move = False
+        while not move:  # while next move not found
+            ind = self.currentState.nextChildToVisit
+            if len(possible_moves) <= ind:  # no moves possible from this node (this is a leaf)
+                if self.currentState.parent:  # try finding ancestor with unvisited children
+                    self.gm.reverseMove(self.currentState.requiredMovable)  # reverse in gm
+                    possible_moves = self.gm.getMovables()  # change list of moves to ancestor's
+                    self.currentState = self.currentState.parent  # become ancestor, retry finding move
+                    continue
+                else:  # no more parents and no more unvisited children, it's gg
+                    return False
+            trying_move = possible_moves[ind]
+            self.gm.makeMove(trying_move)
+            new_state = GameState(self.gm.getGameState, child_depth, trying_move)
+            if new_state not in self.visited:  # this is an unvisited state, explore
                 new_state.parent = self.currentState  # save the current game state as the parent
                 self.currentState.children.append(new_state)
-            self.gm.reverseMove(move)
-        if self.currentState.children:  # there are new unvisited children, expand into the first one
-            self.currentState.nextChildToVisit += 1
-            self.currentState = self.currentState.children[0]
-            if self.currentState == self.victoryCondition:
-                return True
-            else:
-                self.visited[self.currentState] = True
-                return False
-        else:  # no unvisited child node found, find ancestor with unvisited child
-            while self.currentState.parent:  # while there is a parent to go to (i.e. not root)
-                self.currentState = self.currentState.parent
-                ind = self.currentState.nextChildToVisit
-                if len(self.currentState.children) > ind:  # this parent still has unvisited children
-                    self.currentState.nextChildToVisit += 1
-                    self.currentState = self.currentState.children[ind]
-                    if self.currentState == self.victoryCondition:
-                        return True
-                    else:
-                        self.visited[self.currentState] = True
-                        return False
-            return False  # no more unexplored game states
+                self.currentState.nextChildToVisit += 1  # so that next time, continue down possible moves
+                self.currentState = new_state  # move to new state
+                move = trying_move
+            else:  # this state has been visited, go to next possible move at this node
+                self.gm.reverseMove(trying_move)  # return to current node in order to try different move
+                self.currentState.nextChildToVisit += 1
+        if self.currentState.state == self.victoryCondition:  # move found, check if won
+            return True
+        else:
+            self.visited[self.currentState] = True  # have not won, add this state to visited
+            return False
 
 class SolverBFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
